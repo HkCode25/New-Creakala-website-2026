5bad918be88b9bc24f499c3beff2e730177ab620
HkCode25 <hardeepk_fr@yahoo.fr>
2026-09-07 21:17:32 -0600

    new

---
+++ creakala2026react/src/components/Banner.jsx
@@ -0,0 +1,20 @@
+function Banner(){
+    return(
+        <div className="banner">
+            <div><h1>Minimalistic Design and Intuitive Interactions</h1></div>
+            <div><h3>I design and develop sophisticated, user-friendly responsive web applications that transform complex ideas into elegant digital experiences.</h3></div>
+
+        {/* <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button> */}
+
+        </div>
+
+    );
+};
+
+export default Banner;
---
+++ creakala2026react/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+    return(
+        <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
